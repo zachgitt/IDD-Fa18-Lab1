7f7538e7ec262f539a5de50f65fd35074390294f
Zachary Gittelman <zachgitt@umich.edu>
2019-09-09 20:42:24 -0400

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -27,13 +27,13 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(1, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(10);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(10);                       // wait for a second
+  digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(1, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
 }
